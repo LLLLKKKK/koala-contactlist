e1f609930cabc17100e37699fbd181b273301938
LLLLKKKK <kan.liu.229@gmail.com>
2013-01-24 21:14:45 -0800

    photo upload finished

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 var express = require('express')
   , mongoose = require('mongoose')
+  , crypto = require('crypto')
+  , fs = require('fs')
   , routes = require('./routes')
   , group = require('./routes/group')
   , contact = require('./routes/contact')
@@ -16,7 +18,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.set('image types', ['image/png', 'image/gif', 'image/pjpeg', 'image/jpeg']);
-  app.set('image size', 200 * 1024);
+  app.set('image size', 1024);
 
   app.use(express.favicon());
   app.use(express.logger('dev'));
@@ -54,8 +56,6 @@ models.defineModels(mongoose, function() {
   db = mongoose.connect(app.get('db-uri'));
 });
 
-app.get('/', routes.index);
-
 group.defineRoutes(mongoose, function() {
   app.post('/groups', group.create);
   app.get('/groups', group.getAll);
@@ -72,35 +72,37 @@ contact.defineRoutes(mongoose, function() {
   app.del('/contacts/:id', contact.deleteById);
 });
 
-app.post('/photo', function(req, res) {
-  var new_name = (function() {
-    var formal_name = req.files.photo.name;
-    var e_name = formal_name.substring(formal_name.lastIndexOf('.'), formal_name.length);
-    var d = new Data();
-    var p_name = crtyto.createHash('sha1').update(formal_name + d.getTime());
+app.get('/', routes.index);
 
-    return p_name + e_name;
-  })();
+app.post('/photo', function(req, res, next) {
+  if (req.files && req.files.photo) {
+    if (req.files.photo.size <= app.get('image size') &&
+      app.get('image types').indexOf(req.files.photo.type) != -1 ) {
 
-  var tmp_path = req.files.photo.path;
-  var target_path = './public/images/' + new_name;
-  var link = '/images/' + new_name;
+      var new_name = function() {
+        var formal_name = req.files.photo.name;
+        var e_name = formal_name.substring(formal_name.lastIndexOf('.'), formal_name.length);
+        var d = new Date();
+        var sha1 = crypto.createHash('sha1').update(formal_name + d.getTime());
+        var p_name = sha1.digest('hex');
+        return p_name + e_name;
+      }();
 
-  if (req.files.photo.size <= app.get('image size') &&
-    app.get('image types').indexOf(req.files.photo.size) != -1 ) {
-    fs.rename(tmp_path, target_path, function(err) {
-      if (err) throw err;
-
-      fs.unlink(tmp_path, function() {
-        if (err) throw err;
-        res.send(link);
+      var target_path = './public/images/upload/' + new_name;
+      var link = '/images/upload/' + new_name;
+      fs.rename(req.files.photo.path, target_path, function(err) {
+        if (!err) {
+          res.send(link);
+        } else {
+          fs.unlink(req.files.photo.path);
+          next(new Error('Invalid'));
+        }
       });
-    });
+    } else {
+      next(new Error('Invalid'))
+    }
   } else {
-    fs.unlink(tmp_path, function(err) {
-      if (err) throw err;
-      res.send('invalid');
-    })
+    next(new Error('Invalid'))
   }
 
 });
